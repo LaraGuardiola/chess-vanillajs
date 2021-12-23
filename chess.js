be7ef6d8a9ad7f9419093b3000df9f871accdf9c
LaraGuardiola <webdev.slara@gmail.com>
2021-12-23 23:26:35 +0100

    draggable items cannot be dropped if target has children

--- chess.js
+++ chess.js
@@ -1,11 +1,20 @@
 let dragged;
-let lastMoved;
-let listen = (...args) => document.addEventListener(...args);
+let lastMovedStartPos;
+let lastMovedEndPos;
+let tiles = document.querySelectorAll('.box')
+let fichas = [].slice.call(document.querySelectorAll('.box'))
+console.log(document)
+let listen = (...args) => fichas.forEach(tile => tile.addEventListener(...args));
+
+//*EVENTS
 
 listen('dragstart', event => {
 	dragged = event.target; //the piece in this case
-  lastMoved = dragged.parentNode;
-  lastMoved.style.border = "3px solid black"
+  lastMovedStartPos = dragged.parentNode;
+  lastMovedStartPos.style.border = "3px solid black"
+  if(lastMovedEndPos != null){
+    lastMovedEndPos.style.removeProperty("border")
+  }
 }, false)
 
 listen('dragover', event => {
@@ -21,13 +30,13 @@ listen('dragleave', event => {
 
 listen('drop', event => {
 	event.preventDefault();
-	event.target.classList.remove("ondragover")
-  event.target.style.border = "3px solid black"
-  lastMoved.style.removeProperty("border")
+  lastMovedEndPos = event.target
+	lastMovedEndPos.classList.remove("ondragover")
+  lastMovedEndPos.style.border = "3px solid black"
+  lastMovedStartPos.style.removeProperty("border")  
 
-
-	if (event.target.className === 'box') { //if you are over a chess tile and drop it then removes the child from the parent and appends to the selected tile
-		dragged.parentNode.removeChild(dragged);
+  if (lastMovedEndPos.className === 'box' && lastMovedEndPos.childElementCount === 0) { //if you are over a chess tile and drop it then removes the child from the parent and appends to the selected tile
+    dragged.parentNode.removeChild(dragged);
 		event.target.appendChild(dragged);  
 	}
 }, false)
