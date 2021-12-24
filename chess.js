9a0a7cca91759e64313341c3746aa9d242d5d753
LaraGuardiola <webdev.slara@gmail.com>
2021-12-24 19:00:28 +0100

    collisions in nested divs avoided

--- chess.js
+++ chess.js
@@ -2,11 +2,13 @@ let dragged;
 let lastMovedStartPos;
 let lastMovedEndPos;
 let tiles = document.querySelectorAll('.box')
+let pieces = document.querySelectorAll('.piece')
 let listen = (...args) => tiles.forEach(tile => tile.addEventListener(...args));
 
 //*EVENTS
 
 tiles.forEach(() =>{
+  
   listen('dragstart', ondragstart, false)
   listen('dragover', ondragover, false)
   listen('dragleave', ondragleave, false)
@@ -16,9 +18,10 @@ tiles.forEach(() =>{
 //*FUNCTIONS
 
 function ondragstart(event){
-  dragged = event.target; //the piece in this case
+  dragged = event.target;
   lastMovedStartPos = dragged.parentNode;
   lastMovedStartPos.style.border = "3px solid black"
+  //first time playing lastMovedEndPos is not initialized so this if is necessary to catch the undefined it was going to give otherwise
   if(lastMovedEndPos != null){
     lastMovedEndPos.style.removeProperty("border")
   }
@@ -26,10 +29,12 @@ function ondragstart(event){
 
 function ondragover(event){
   event.preventDefault()
-  if(event.target.className === 'box' || 'piece' && event.target.childElementCount === 0) return
-  else{
-    event.target.classList.add("ondragover")
-  }
+  if (event.target.getAttribute("draggable") == "true" || event.target.hasChildNodes()){
+    event.dataTransfer.dropEffect = "none"; // dropping is not allowed
+  }else{
+    event.dataTransfer.dropEffect = "all";
+        event.target.classList.add("ondragover") // drop it
+  }        
 }
 
 function ondragleave(event){
@@ -43,7 +48,8 @@ function ondrop(event){
   lastMovedEndPos.style.border = "3px solid black"
   lastMovedStartPos.style.removeProperty("border")  
 
-  if (lastMovedEndPos.className === 'box' && lastMovedEndPos.childElementCount === 0) { //if you are over a chess tile and drop it then removes the child from the parent and appends to the selected tile
+  //if you are over a chess tile and drop it then removes the child from the parent and appends to the selected tile
+  if (lastMovedEndPos.className === 'box' && lastMovedEndPos.childElementCount === 0) { 
     dragged.parentNode.removeChild(dragged);
 		event.target.appendChild(dragged);  
 	}
