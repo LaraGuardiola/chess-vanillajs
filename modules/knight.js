0f9fbd0598d8b46c0eca9feb2b889db80a004944
LaraGuardiola <webdev.slara@gmail.com>
2022-01-27 23:15:30 +0100

    pawn and rook modularized

---
+++ modules/knight.js
@@ -0,0 +1,90 @@
+import * as util from './util.js'
+
+  //* KNIGHT */
+export function knightMovement(event){
+  let knightMovements = [-17,-15,-10,-6,6,10,15,17]
+  let indexPiece = util.tilesArray.indexOf(event.target.parentNode)
+  let type = event.target.className
+  let color
+
+  if(type.includes('white')) color = 'white'
+  else color = 'black'
+
+  //Necessary in order to avoid unwanted movements to the other extreme of the board
+  if(isInFirstColumn(indexPiece)) knightMovements = [-17,-10,6,15]
+  if(isInSecondColumn(indexPiece)) knightMovements = [-17,-15,-10,6,15,17]
+  if(isInSeventhColumn(indexPiece)) knightMovements = [-17,-15,-6,10,15,17]
+  if(isInEighthColumn(indexPiece)) knightMovements = [-15,-6,10,17]
+  
+  knightMovements.forEach(move => {
+    if(!isOutOfBounds(indexPiece - move)){
+      util.tilesArray[indexPiece - move].classList.add('ondragstart')
+    }
+    util.tilesArray.forEach(tile => {
+      if(tile.hasChildNodes() && tile.firstChild.classList.contains(color)){
+        tile.classList.remove('ondragstart')
+      }
+    })  
+  })
+}
+
+//switch statements are slightly more efficient than ifs statements (0,008 ms vs 0,005)
+export function isInSecondColumn(position){
+  switch(position){
+    case 1:
+    case 9:
+    case 17:
+    case 25:
+    case 33:
+    case 41:
+    case 49:
+    case 57:
+      return true
+  }
+}
+
+export function isInFirstColumn(position){
+  switch(position){
+    case 0:
+    case 8:
+    case 16:
+    case 24:
+    case 32:
+    case 40:
+    case 48:
+    case 56:
+      return true
+  }
+}
+
+export function isInSeventhColumn(position){
+  switch(position){
+    case 6:
+    case 14:
+    case 22:
+    case 30:
+    case 38:
+    case 46:
+    case 54:
+    case 62:
+      return true
+  }
+}
+
+export function isInEighthColumn(position){
+  switch(position){
+    case 7:
+    case 15:
+    case 23:
+    case 31:
+    case 39:
+    case 47:
+    case 55:
+    case 63:
+      return true
+  }
+}
+
+export function isOutOfBounds(movement){
+  return movement > 63 || movement < 0 ? true : false
+}
---
+++ modules/rook.js
@@ -0,0 +1,130 @@
+import * as util from './util.js'
+  
+  //* ROOK */
+
+let color
+
+//checks if inside the tiles of a row there's a piece
+export function checkRowCollision(rowIndex,index){
+  return util.rows[rowIndex][index].hasChildNodes() ? true : false
+}
+
+//checks if inside the tiles of a column there's a piece
+export function checkColumnCollision(arrayColumn,index){
+  return arrayColumn[index].hasChildNodes() ? true : false
+}
+
+export function createRows(arr, numGroups) {
+  const perGroup = Math.ceil(arr.length / numGroups)   //in this case, 8 per group (8x8 = 64 tiles)
+  return new Array(numGroups)
+    .fill('')
+    .map((_, i) => arr.slice(i * perGroup, (i + 1) * perGroup))
+}
+
+export function getColumn(event){
+  let indexPiece = util.tilesArray.indexOf(event.target.parentNode)
+  let rowPos = Math.floor(indexPiece / 8)
+  let indexRow = util.rows[rowPos].indexOf(event.target.parentNode)
+  let column = []
+
+  if(column.length === 8){ //without this, every time you drag the piece it keeps pushing
+    return
+  }else{
+    for(let row of util.rows){
+      column.push(row[indexRow])
+    }
+    return column
+  }
+}
+
+export function rookMovement(event){
+  let type = event.target.className
+  if(type.includes('white')) color = 'white'
+  else color = 'black'
+  rookHorizontalMove(event)
+  rookVerticalMove(event)
+}
+
+export function printMovementTiles(movementArray){
+  movementArray.forEach(tile => {
+    if(!tile.hasChildNodes()){
+      tile.classList.add('ondragstart')
+    }
+  })
+}
+
+export function rookHorizontalMove(event){
+  let indexPiece = util.tilesArray.indexOf(event.target.parentNode)
+  let rowPos = Math.floor(indexPiece / 8)
+  let row = util.rows[rowPos]
+  let indexRow = util.rows[rowPos].indexOf(event.target.parentNode)
+  let nonEmptySpaces = []
+
+  for (let i = 0; i < row.length; i++) { //returns an array with the indexes where pieces are placed
+    if (row[i].hasChildNodes())
+        nonEmptySpaces.push(i);
+  }
+
+  let pieceRelativeToOthers = nonEmptySpaces.indexOf(indexRow) //gives index position for the array of pieces
+  let movementArray = []
+
+  if (pieceRelativeToOthers === 0)
+    movementArray = row.slice(0, nonEmptySpaces[pieceRelativeToOthers + 1])
+  else {
+    movementArray = row.slice(nonEmptySpaces[pieceRelativeToOthers - 1] + 1, nonEmptySpaces[pieceRelativeToOthers + 1])
+  }
+
+  let distanceBetweenLeft = nonEmptySpaces[pieceRelativeToOthers] - nonEmptySpaces[pieceRelativeToOthers - 1]
+  let distanceBetweenRight = nonEmptySpaces[pieceRelativeToOthers + 1] - nonEmptySpaces[pieceRelativeToOthers]
+
+  if(isNaN(distanceBetweenLeft)){ //otherwise index could be less than 0, giving error
+    distanceBetweenLeft = 0
+  }
+  if(isNaN(distanceBetweenRight)){ //like before, if index is out of bound is going to return an error
+    distanceBetweenRight = 0
+  }
+  if(checkRowCollision(rowPos, indexRow - distanceBetweenLeft) && !util.tilesArray[indexPiece - distanceBetweenLeft].firstChild.classList.contains(color)){
+    util.tilesArray[indexPiece - distanceBetweenLeft].classList.add('ondragstart')
+  }
+  if(checkRowCollision(rowPos, indexRow + distanceBetweenRight) && !util.tilesArray[indexPiece + distanceBetweenRight].firstChild.classList.contains(color)){
+    util.tilesArray[indexPiece + distanceBetweenRight].classList.add('ondragstart')
+  }
+  printMovementTiles(movementArray)
+}
+
+export function rookVerticalMove(event){
+  let column = getColumn(event)
+  let nonEmptySpaces = []
+  let indexColumn = column.indexOf(event.target.parentNode)
+
+  for (let i = 0; i < column.length; i++) { 
+    if (column[i].hasChildNodes())
+        nonEmptySpaces.push(i);
+  }
+
+  let pieceRelativeOthers = nonEmptySpaces.indexOf(indexColumn) //gives index position for the array of pieces
+  let movementArray = []
+
+  if (pieceRelativeOthers === 0)
+    movementArray = column.slice(0, nonEmptySpaces[pieceRelativeOthers + 1])
+  else {
+    movementArray = column.slice(nonEmptySpaces[pieceRelativeOthers - 1] + 1, nonEmptySpaces[pieceRelativeOthers + 1])
+  }
+
+  let distanceBetweenLeft = nonEmptySpaces[pieceRelativeOthers] - nonEmptySpaces[pieceRelativeOthers - 1]
+  let distanceBetweenRight = nonEmptySpaces[pieceRelativeOthers + 1] - nonEmptySpaces[pieceRelativeOthers]
+
+  if(isNaN(distanceBetweenLeft)){ //otherwise index could be less than 0, giving error
+    distanceBetweenLeft = 0
+  }
+  if(isNaN(distanceBetweenRight)){ //like before, if index is out of bound is going to return an error
+    distanceBetweenRight = 0
+  }
+  if(checkColumnCollision(column, indexColumn - distanceBetweenLeft) && !column[indexColumn - distanceBetweenLeft].firstChild.classList.contains(color)){
+    column[indexColumn - distanceBetweenLeft].classList.add('ondragstart')
+  }
+  if(checkColumnCollision(column, indexColumn + distanceBetweenRight) && !column[indexColumn + distanceBetweenRight].firstChild.classList.contains(color)){
+    column[indexColumn + distanceBetweenRight].classList.add('ondragstart')
+  }
+  printMovementTiles(movementArray)
+}
