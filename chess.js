0015b44cab91bab9583e419c96c7a35424cc3752
LaraGuardiola <webdev.slara@gmail.com>
2022-01-28 13:59:08 +0100

    removed unused ontouchevent

--- chess.js
+++ chess.js
@@ -5,7 +5,6 @@ import * as ui from './modules/ui.js'
 util.tiles.forEach(() =>{
   util.setEvent('click', ui.cleanBoard,false) 
   util.setEvent('dragstart', events.ondragstart, false)
-  util.setEvent('touchstart',events.ondragstart, false)
   util.setEvent('dragover', events.ondragover, false)
   util.setEvent('drop', events.ondrop, false)
 })
